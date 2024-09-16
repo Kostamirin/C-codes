060e30e419fc66ec3bc0457d8b549f0e8e2baac9
KostikMirin <132436003+Kostamirin@users.noreply.github.com>
2024-09-16 13:16:34 +0300

    Update Reverse Integer
    done. Again

--- LeetCode/Reverse Integer( Задание 7, переворот челых чисел).cpp
+++ LeetCode/Reverse Integer( Задание 7, переворот челых чисел).cpp
@@ -2,20 +2,15 @@
 #include <string>
 #include <algorithm>
 
-
-
 class Solution {
 public:
     int reverse(int x) {
-        if(x == 0){return x;}
-        
+        if(x == 0){return x;}        
         bool it_is_minus_number;
         if(x < 0){it_is_minus_number = true;}
         else{it_is_minus_number = false;}
-
         std::string something = std::to_string(x);
         std::reverse(something.begin(), something.end());
-
         int i = 0;
         if(something[0] == '0'){
             do{
@@ -34,22 +29,5 @@ public:
         catch(std::out_of_range){
             return 0;
         }
-
     };
 };
-
-
-
-
-
-
-#include <iostream>
-int main(){
-    int x = 1534236469;
-    Solution solution;
-    int result = solution.reverse(x);
-    std::cout <<x <<std::endl; 
-    std::cout <<result <<std::endl;
-    std::cin.get();
-    return 0;
-}
