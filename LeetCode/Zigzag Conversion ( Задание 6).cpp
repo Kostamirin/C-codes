ac1df1695282ce7ba788cbc5297c41cd81483ce1
KostikMirin <132436003+Kostamirin@users.noreply.github.com>
2024-09-16 13:07:58 +0300

    files update
    Here is update of 7th problem of Leetcode

--- LeetCode/Zigzag Conversion ( Задание 6).cpp
+++ LeetCode/Zigzag Conversion ( Задание 6).cpp
@@ -1,55 +1,65 @@
 #include <string>
 #include <vector>
 
-// Создает первый столб букавок
-void first(std::string s, int numRows, std::vector <char> something, int position){
-    for(int i = 0; i <= numRows; i++){
-        position ++;
-        something.push_back(s[position]);
-    }
-    // после этого определяется размер вектора который должен быть статичный и не изменяем
-}
+//!         КОД НЕ РАБОТАЛ ИСПОЛЬЗУЯ ФУНКЦИИ НАХОДИЩИЕСЯ ВНЕ КЛАССА
+//!         КОД НЕ РАБОТАЛ ИСПОЛЬЗУЯ ФУНКЦИИ НАХОДИЩИЕСЯ ВНЕ КЛАССА
 
-//! дальше должна быть функция которая имеет i = numRows - 1 || -- в зависимости от условия и идти в низ по числам и вверх на условном графике
-void up(std::string s, int numRows, std::vector <char> something, int position){
-    for(int i = numRows - 1; i >= 0; i--){
-        position ++;
-        something[i] += s[position];
-    }
-}
 
-void down(std::string s, int numRows, std::vector <char> somethign, int position){
-    for(int i = 1; i < numRows; i++){
-        position ++;
-        somethign[i] += s[position];
-    }
-}
 
 class Solution {
-public:
+public: 
     std::string convert(std::string s, int numRows) {
-        
-        if (numRows == 1) return s;
-        if (s.size() == 1) return s;
-        
-        std::vector <char> something;// это должен быть строки/строки отвечающие за сохранение символов
+        if (numRows == 1 || s.size() == 1) return s;
+
+        std::vector <std::string> something(numRows);// это должен быть строки/строки отвечающие за сохранение символов
         
         // следующая переменная должна отвечать за позицию которая у меня есть для отсчета 
         int position = 0;
-        first(s,numRows, something, position);
-        do{
-            up(s, numRows, something, position);
-            down(s,numRows, something, position);
-        }while(position !=s.size());
+        while(position != s.size())
+        {
+            for(int i = 0; i < numRows; i++){
+                //Если позиция не привышает размер текста то зиг-заг будет продолжаться
+                if(position < s.size()){
+                    something[i] += s[position];
+                    position ++;
+                }
+            }
+            //! Не знаю почему здесь испоьзуется -2, но именно из-за этого все заработало!
+            for(int i = numRows - 2; i > 0; i--){
+                // Если позиция не больше размера текста то код будет продолжаться, иначе зиг-заг не дойдет до конца
+                if(position < s.size()){
+                    something[i] += s[position];
+                    position ++;
+                }
+            }
+        }
     
     // после этого нужно сделать строку из вектора
     //!std::string result(something.begin(), something.end());
     
     std::string result;
-    for (char c : something) {
-        result += c;
+
+    for(int i = 0; i < something.size(); i++){
+        result += something[i];
     }
+
 
     return result;
     }
 };
+#include <iostream>
+// Добавляем функцию main для локального тестирования
+int main() {
+    std::string something = "skeaitmsngsueisk";
+    int rows = 5;
+
+    // Создаем объект класса Solution
+    Solution solution;
+    // Передаем тестовые значения в функцию convert
+    std::string result = solution.convert(something, rows);
+
+    // Вывод результата
+    std::cout << "Result: " << result << std::endl;
+
+    return 0;
+}
