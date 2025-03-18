e1045ea79a95b25c13ec2819bb652f87ffe5aa7e
KerasinChick <base@gmain.com>
2025-03-18 18:46:51 +0300

    Fix input handling and refine known values logic
    Corrected input prompts and addressed input handling issues to avoid unintentional value changes. Improved the logic for managing known values by simplifying the loop and fixing conditions to prevent errors with null values.

--- C++ (personal_works)/Прогрессия_арифметическая_и_геометрическая.h
+++ C++ (personal_works)/Прогрессия_арифметическая_и_геометрическая.h
@@ -42,21 +42,22 @@ class base
                     "2.Exit\n\n";
         std::cout<<"Enter your choice:";int choice; std::cin>>choice;
         switch (choice){case 2: first(a,n,d,known_a, known_sum); break; case 1: break; default: std::cout<<"Error: Wrong choice"; break;}
-        std::cout<<"Enter known values(if ih net=propusk):";
-        std::cout<<"a: ";std::cin>>a;
+        std::cout<<"Enter known values(if ih net=propusk)\n";
+        std::cout<<"a: ";std::cin>>a;                       //! ОШИБКА при вводе Enter - значение не может не измениться
         std::cout<<"n: ";std::cin>>n;
         std::cout<<"d: ";std::cin>>d;
-        std::cout<<"known_a: ";std::cin>>known_a;
+        std::cout<<"last a: ";std::cin>>known_a;            //! Стоит создать запрос на любую другую известную a, типа a8,a14, a7, и типа того
         std::cout<<"known_sum: ";std::cin>>known_sum;
 
         //Создание массива известных значений
         std::map<std::string,int> known_values{{"a",a},{"n", n},{"d", d},
                                                {"known_a", known_a},{"known_sum", known_sum}};
-        for (int i = 0; i < known_values.size(); i++)
+        for (const auto& values : known_values)
         {
-            if (std::find_if(known_values.begin(), known_values.end(), [](std::pair<std::string,int>value, bool task){return value.second == NULL;}))
+            if (values.second == NULL)
             {
-
+                known_values.erase(values.first);
+                //known_values.erase(values.second);
             }
         }
 
