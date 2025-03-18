59acd30f61f147ad3d61779f52eacee8e06b094e
KerasinChick <base@gmain.com>
2025-03-18 18:07:58 +0300

    Первые изменения __update

--- C++ (personal_works)/Прогрессия_арифметическая_и_геометрическая.h
+++ C++ (personal_works)/Прогрессия_арифметическая_и_геометрическая.h
@@ -1,11 +1,51 @@
 // Created by kleymuner2131 on 18.03.25.
-
+#include <iostream>
 #ifndef ПРОГРЕССИЯ_АРИФМЕТИЧЕСКАЯ_И_ГЕОМЕТРИЧЕСКАЯ_H
 #define ПРОГРЕССИЯ_АРИФМЕТИЧЕСКАЯ_И_ГЕОМЕТРИЧЕСКАЯ_H
 
 
+class base
+{
+    public:
+    void first(int a, int n, int d, int known_a, int known_sum)
+    {
+        std::cout<<"1.Arifmetical progression\n"
+                   "2.Geometric progression\n"
+                   "3.Instruction\n";
+                   "4.Exit\n\n";
+        std::cout<<"Enter your choice:";
+        int choice;
+        std::cin>>choice;
+        switch (choice)
+        {
+        case 1: arifmetical(a,n,d,known_a, known_sum);
+            break;
+        case 2: geometric();
+            break;
+        case 3: instruction();
+            break;
+        case 4: exit(0);
+            break;
+        default:
+            std::cout<<"Error: Wrong choice";
+            exit(0);
+            break;
+        }
+    }
+    void arifmetical(int a,int n,int d,int known_a, int known_sum)
+    {
+        //* Запрос имеющийся значений
 
-class Прогрессия_арифметическая_и_геометрическая {
+
+    }
+    void geometric()
+    {
+
+    }
+    void instruction()
+    {
+
+    }
 
 };
 
