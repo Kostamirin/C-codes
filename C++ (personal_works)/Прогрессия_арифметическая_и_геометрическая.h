196ae0897f8913ec3d1a8f2b015d460cc52facf3
KerasinChick <base@gmain.com>
2025-03-18 18:32:41 +0300

    Add input handling and known values map in progression logic
    Improved functionality by implementing user input prompts for known values in "arifmetical" function. Added a map to store and manage provided values effectively, laying the groundwork for further validation and computation. Included relevant headers to support new data structures and algorithms.

--- C++ (personal_works)/Прогрессия_арифметическая_и_геометрическая.h
+++ C++ (personal_works)/Прогрессия_арифметическая_и_геометрическая.h
@@ -1,5 +1,8 @@
 // Created by kleymuner2131 on 18.03.25.
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
 #ifndef ПРОГРЕССИЯ_АРИФМЕТИЧЕСКАЯ_И_ГЕОМЕТРИЧЕСКАЯ_H
 #define ПРОГРЕССИЯ_АРИФМЕТИЧЕСКАЯ_И_ГЕОМЕТРИЧЕСКАЯ_H
 
@@ -11,7 +14,7 @@ class base
     {
         std::cout<<"1.Arifmetical progression\n"
                    "2.Geometric progression\n"
-                   "3.Instruction\n";
+                   "3.Instruction\n"
                    "4.Exit\n\n";
         std::cout<<"Enter your choice:";
         int choice;
@@ -35,6 +38,28 @@ class base
     void arifmetical(int a,int n,int d,int known_a, int known_sum)
     {
         //* Запрос имеющийся значений
+        std::cout<<"1.Enter known values\n"
+                    "2.Exit\n\n";
+        std::cout<<"Enter your choice:";int choice; std::cin>>choice;
+        switch (choice){case 2: first(a,n,d,known_a, known_sum); break; case 1: break; default: std::cout<<"Error: Wrong choice"; break;}
+        std::cout<<"Enter known values(if ih net=propusk):";
+        std::cout<<"a: ";std::cin>>a;
+        std::cout<<"n: ";std::cin>>n;
+        std::cout<<"d: ";std::cin>>d;
+        std::cout<<"known_a: ";std::cin>>known_a;
+        std::cout<<"known_sum: ";std::cin>>known_sum;
+
+        //Создание массива известных значений
+        std::map<std::string,int> known_values{{"a",a},{"n", n},{"d", d},
+                                               {"known_a", known_a},{"known_sum", known_sum}};
+        for (int i = 0; i < known_values.size(); i++)
+        {
+            if (std::find_if(known_values.begin(), known_values.end(), [](std::pair<std::string,int>value, bool task){return value.second == NULL;}))
+            {
+
+            }
+        }
+
 
 
     }
